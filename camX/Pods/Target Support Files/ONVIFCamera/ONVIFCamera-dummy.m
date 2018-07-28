99c17825f45d8c5ab862a5c61a6fea4153620b5e
Kotwal <d.kotwal@sap.com>
2018-07-28 10:04:58 +0530

    blockchain solution

---
+++ camX/Pods/Target Support Files/ONVIFCamera/ONVIFCamera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ONVIFCamera : NSObject
+@end
+@implementation PodsDummy_ONVIFCamera
+@end
---
+++ camX/Pods/Target Support Files/ONVIFCamera/ONVIFCamera-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "SOAPEngine.h"
+
+FOUNDATION_EXPORT double ONVIFCameraVersionNumber;
+FOUNDATION_EXPORT const unsigned char ONVIFCameraVersionString[];
+
